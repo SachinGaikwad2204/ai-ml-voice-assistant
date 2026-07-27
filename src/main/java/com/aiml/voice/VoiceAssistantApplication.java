a5bd4485f87ceea1a09a4275500677c3da8b84a0
Sachin Gaikwad <sachingaikwad2204@gmail.com>
2026-07-27 18:28:27 +0530

    Remove H2 driver, force PostgreSQL

--- src/main/java/com/aiml/voice/VoiceAssistantApplication.java
+++ src/main/java/com/aiml/voice/VoiceAssistantApplication.java
@@ -1,8 +1,7 @@
-﻿package com.aiml.voice;
+package com.aiml.voice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {
